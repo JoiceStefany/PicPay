df612d1a3050d0535156aaf6f9b354fa61421cfe
JoiceStefany <steffanyjoice006@gmail.com>
2023-11-10 15:15:07 -0300

    Initial Project

---
+++ src/test/java/com/PicPay/picpay/PicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PicPay.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
